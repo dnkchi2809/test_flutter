b9562d10ee9929c02c977a0819085d0755a4d820
Kim Chi <45139505+dnkchi2809@users.noreply.github.com>
2024-06-06 17:12:43 +0700

    feat: focus.dart

---
+++ lib/focus.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyCustomWidget extends StatefulWidget {
+  const MyCustomWidget({super.key});
+
+  @override
+  State<MyCustomWidget> createState() => _MyCustomWidgetState();
+}
+
+class _MyCustomWidgetState extends State<MyCustomWidget> {
+  Color _color = Colors.white;
+  String _label = 'Unfocused';
+
+  @override
+  Widget build(BuildContext context) {
+    return Focus(
+      onFocusChange: (focused) {
+        setState(() {
+          _color = focused ? Colors.black26 : Colors.white;
+          _label = focused ? 'Focused' : 'Unfocused';
+        });
+      },
+      child: Center(
+        child: Container(
+          width: 300,
+          height: 50,
+          alignment: Alignment.center,
+          color: _color,
+          child: Text(_label),
+        ),
+      ),
+    );
+  }
+}
+
+class OrderedButtonRow extends StatelessWidget {
+  const OrderedButtonRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FocusTraversalGroup(
+      policy: OrderedTraversalPolicy(),
+      child: Row(
+        children: <Widget>[
+          const Spacer(),
+          FocusTraversalOrder(
+            order: const NumericFocusOrder(2),
+            child: TextButton(
+              child: const Text('ONE'),
+              onPressed: () {},
+            ),
+          ),
+          const Spacer(),
+          FocusTraversalOrder(
+            order: const NumericFocusOrder(1),
+            child: TextButton(
+              child: const Text('TWO'),
+              onPressed: () {},
+            ),
+          ),
+          const Spacer(),
+          FocusTraversalOrder(
+            order: const NumericFocusOrder(3),
+            child: TextButton(
+              child: const Text('THREE'),
+              onPressed: () {},
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
