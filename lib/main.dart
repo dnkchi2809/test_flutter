1dc35ecc198653cf9ec69fa5c9ea8e890e2a7299
Kim Chi <45139505+dnkchi2809@users.noreply.github.com>
2024-06-06 15:19:41 +0700

    WIP: focus textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -10,7 +12,7 @@ void main() {
         //   backgroundColor: Colors.red,
         //   title: const Text('Home'),
         // ),
-        body: ImageAndCardWidget(),
+        body: TestInput(),
       ),
     ),
     theme: ThemeData(
@@ -20,8 +22,185 @@ void main() {
   ));
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class TestInput extends StatefulWidget {
+  const TestInput({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return TestInputState();
+  }
+}
+
+class TestInputState extends State<TestInput> {
+  late FocusNode MyFocusNode;
+  final ValueNotifier<bool> MyFocusNotifier = ValueNotifier<bool>(false);
+
+  @override
+  void initState() {
+    super.initState();
+
+    MyFocusNode = FocusNode();
+    MyFocusNode.addListener(_onFocusChange);
+  }
+
+  @override
+  void dispose() {
+    MyFocusNode.removeListener(_onFocusChange);
+    MyFocusNode.dispose();
+    MyFocusNotifier.dispose();
+
+    super.dispose();
+  }
+
+  void _onFocusChange() {
+    MyFocusNotifier.value = MyFocusNode.hasFocus;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: MyFocusNotifier,
+      builder: (_, isFocus, child) {
+        return TextField(
+          focusNode: MyFocusNode,
+          decoration: const InputDecoration(
+            hintText: "hintText",
+            filled: true,
+            fillColor: isFocus ? Colors.pink : Colors.green,
+          ),
+          onChanged: (value) => print(value),
+        );
+      },
+    );
+  }
+}
+
+class MyTextWidget extends StatefulWidget {
+  const MyTextWidget({
+    super.key,
+    // this.onChanged,
+    // this.fillColor =
+    //     const Color.fromRGBO(240, 240, 240, 1), // Selects a mid-range green.,
+    // this.focusColor = const Color.fromRGBO(140, 240, 240, 1),
+  });
+
+  // final ValueChanged<String>? onChanged;
+  // final Color? fillColor;
+  // final Color? focusColor;
+
+  @override
+  State<MyTextWidget> createState() => _MyTextWidgetState();
+}
+
+class _MyTextWidgetState extends State<MyTextWidget> {
+  late FocusNode _myFocusNode;
+  final ValueNotifier<bool> _myFocusNotifier = ValueNotifier<bool>(false);
+
+  // final myController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _myFocusNode = FocusNode();
+    _myFocusNode.addListener(_onFocusChange);
+    // myController.addListener(_printLatestValue);
+  }
+
+  @override
+  void dispose() {
+    _myFocusNode.removeListener(_onFocusChange);
+    _myFocusNode.dispose();
+    _myFocusNotifier.dispose();
+    // myController.dispose();
+
+    super.dispose();
+  }
+
+  void _onFocusChange() {
+    _myFocusNotifier.value = _myFocusNode.hasFocus;
+  }
+
+  // void _printLatestValue() {
+  //   final text = myController.text;
+  //   print('Second text field: $text (${text.characters.length})');
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: _myFocusNotifier,
+      builder: (_, isFocus, child) {
+        return TextField(
+          // controller: myController,
+          focusNode: _myFocusNode,
+          decoration: InputDecoration(
+              label: Text("Label"),
+              hintText: "hintText",
+              filled: true,
+              fillColor: isFocus ? Colors.green : Colors.red,
+              helperText: 'helperText'),
+          onChanged: (value) => print(value),
+          keyboardType: TextInputType.number,
+          maxLength: 10,
+        );
+      },
+    );
+  }
+}
+
+class TextFieldWidget extends StatelessWidget {
+  const TextFieldWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      width: 1000,
+      child: Column(
+        children: [
+          const TextField(
+            decoration: InputDecoration(
+              // label: Text('TextField'),
+              icon: Icon(Icons.icecream_outlined),
+              border: OutlineInputBorder(),
+              hintText: 'Enter a search term',
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          TextFormField(
+            autofocus: true,
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter your username',
+              labelStyle: TextStyle(color: Colors.red),
+              focusedBorder: OutlineInputBorder(),
+              fillColor: Colors.grey,
+              filled: true,
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const TextField(
+              decoration: InputDecoration(
+            hintText: 'Hint Text',
+            errorText: 'Error Text',
+            border: OutlineInputBorder(),
+            contentPadding:
+                EdgeInsets.only(left: 11, right: 3, top: 14, bottom: 104),
+            errorStyle: TextStyle(fontSize: 9, height: 5.3),
+          )),
+        ],
+      ),
+    );
+  }
+}
+
+class TestStatelessWidget extends StatelessWidget {
+  const TestStatelessWidget({super.key});
 
   // final bool loading;
   //
@@ -60,10 +239,10 @@ class MyWidget extends StatelessWidget {
   }
 }
 
-class MyWidget2 extends StatefulWidget {
+class TestStatefulWidget extends StatefulWidget {
   final bool loading;
 
-  const MyWidget2(this.loading, {super.key});
+  const TestStatefulWidget(this.loading, {super.key});
 
   @override
   State<StatefulWidget> createState() {
@@ -71,7 +250,7 @@ class MyWidget2 extends StatefulWidget {
   }
 }
 
-class MyWidget2State extends State<MyWidget2> {
+class MyWidget2State extends State<TestStatefulWidget> {
   late bool _localLoading;
 
   @override
