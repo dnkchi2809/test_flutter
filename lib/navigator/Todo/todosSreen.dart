4ce961a417f9a9f3112281208750721074fd7970
Kim Chi <45139505+dnkchi2809@users.noreply.github.com>
2024-06-07 09:32:23 +0700

    feat: basic todo list

---
+++ lib/navigator/Todo/todosSreen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:test_flutter/navigator/Todo/todo.dart';
+
+import 'detailSreeen.dart';
+
+class TodosScreen extends StatelessWidget {
+  // Requiring the list of todos.
+  final List<Todo> todos;
+
+  const TodosScreen({super.key, required this.todos});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todos'),
+      ),
+      //passing in the ListView.builder
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
